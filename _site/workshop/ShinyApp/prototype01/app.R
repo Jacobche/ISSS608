a2456d24ebe9bddde44fb8a2781c7b0b101aff88
Jacob Che <jacob.che@razer.com>
2022-05-06 11:11:02 +0800

    th ex3 1st edition

---
+++ _site/workshop/ShinyApp/prototype01/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(tidyverse) ##read csv using readr but it is part of tidyverse family
+
+exam <- read.csv("data/Exam_data.csv")
+
+ui <- fluidPage(
+    titlePanel("Pupils Examination Result Dashboard"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "variable",
+                        label = "Subject:",
+                        choices = c("English" = "ENGLISH", #Create a list here
+                                    "Maths" = "MATHS",
+                                    "Science" = "SCIENCE"),
+                        selected = "ENGLISH"), #Default value
+            sliderInput(inputId = "bins",
+                        label = "Number of Bins",
+                        min = 5,
+                        max = 20,
+                        value = 10) #Default value
+        ),
+        mainPanel(
+          plotOutput("distPlot") #A container for the output
+    )
+  )
+)
+
+server <- function(input, output) {
+    output$distPlot <- renderPlot({
+        x <- unlist(exam[,input$variable])
+        ggplot(exam, aes(x)) +
+            geom_histogram(bins = input$bins,
+                          color="black",
+                          fill="light blue")
+    })
+}
+
+shinyApp(ui = ui, server = server)
